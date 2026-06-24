5ea334ce20f42db7348c1b8482946377e984e47d
Mayur Bhai <mayurrakholiya81@gmail.com>
2026-06-24 19:58:47 +0530

    Add files via upload

---
+++ Javascript/23-06-2026/demo.js
@@ -0,0 +1,7 @@
+document.getElementById("firstHeading").innerHTML = "HELLO WORLD";
+
+document.getElementById("paraOne").innerHTML = " HELLO WORLD";
+
+document.getElementById("paraOne").style.color = "RED";
+
+console.log('firstheading')
